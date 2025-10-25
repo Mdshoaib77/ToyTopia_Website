1ce3332c12cbe9eaab85d7fabbfc71989ca5ca42
mdshoaib77 <email4966@gmail.com>
2025-10-25 10:06:27 +0600

    Alhamdulillah

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,183 +1,3 @@
-// // // // import React from 'react'
-// // // // import { Outlet } from 'react-router'
-// // // // import Navbar from '../Components/Navbar'
-// // // // import Footer from '../Components/Footer'
-// // // // import Slider from '../Components/Slider'
-// // // // import Carousel from '../Components/Carousel'
-// // // // import BestSellingToys from '../Components/BestSellingToys'
-// // // // import Testimonials from '../Components/Testimonials'
-
-// // // // const Root = () => {
-// // // //   return (
-// // // //     <div>
-// // // // <Navbar/>
-// // // // <Slider/>
-// // // // <Carousel/>
-// // // // <Outlet/>
-// // // // <BestSellingToys/>
-// // // // <Testimonials/>
-// // // // <Footer/>
-// // // //     </div>
-// // // //   )
-// // // // }
-
-// // // // export default Root
-
-
-// // // import React from 'react';
-// // // import { Outlet, useLocation } from 'react-router';
-// // // import Navbar from '../Components/Navbar';
-// // // import Footer from '../Components/Footer';
-// // // import Slider from '../Components/Slider';
-// // // import Carousel from '../Components/Carousel';
-// // // import BestSellingToys from '../Components/BestSellingToys';
-// // // import Testimonials from '../Components/Testimonials';
-
-// // // const Root = () => {
-// // //   const location = useLocation(); // Get the current route
-
-// // //   // Define the routes where the Slider and Carousel should not be shown
-// // //   const hideLayoutComponents = ['/login', '/register', '/profile'];
-
-// // //   return (
-// // //     <div>
-// // //       {/* Always show Navbar */}
-// // //       <Navbar />
-
-// // //       {/* Conditionally show Slider and Carousel */}
-// // //       {!hideLayoutComponents.includes(location.pathname) && (
-// // //         <>
-// // //           <Slider />
-// // //           <Carousel />
-// // //         </>
-// // //       )}
-
-// // //       {/* Render the page content (Outlet) */}
-// // //       <Outlet />
-
-// // //       {/* Always show BestSellingToys and Testimonials */}
-// // //       {!hideLayoutComponents.includes(location.pathname) && (
-// // //         <>
-// // //           <BestSellingToys />
-// // //           <Testimonials />
-// // //         </>
-// // //       )}
-
-// // //       {/* Always show Footer */}
-// // //       <Footer />
-// // //     </div>
-// // //   );
-// // // };
-
-// // // export default Root;
-
-
-// // import React from 'react';
-// // import { Outlet, useLocation } from 'react-router';
-// // import Navbar from '../Components/Navbar';
-// // import Footer from '../Components/Footer';
-// // import Slider from '../Components/Slider';
-// // import Carousel from '../Components/Carousel';
-// // import BestSellingToys from '../Components/BestSellingToys';
-// // import Testimonials from '../Components/Testimonials';
-// // import HeroSection from '../Components/HeroSection';
-
-// // const Root = () => {
-// //   const location = useLocation(); // Get the current route
-
-// //   // Define the routes where Slider, Carousel, BestSellingToys, and Testimonials should not be shown
-// //   // Checking if path contains '/toy/' (for toy details pages)
-// //   const hideLayoutComponents = ['/login', '/register', '/profile'];
-// //   const isToyDetailsPage = location.pathname.includes('/toy/');
-
-// //   return (
-// //     <div>
-// //       {/* Always show Navbar */}
-// //       <Navbar />
-// //       <HeroSection/>
-
-// //       {/* Conditionally show Slider and Carousel */}
-// //       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
-// //         <>
-// //           <Slider />
-// //           <Carousel />
-// //         </>
-// //       )}
-
-// //       {/* Render the page content (Outlet) */}
-// //       <Outlet />
-
-// //       {/* Always show BestSellingToys and Testimonials, except on certain routes */}
-// //       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
-// //         <>
-// //           <BestSellingToys />
-// //           <Testimonials />
-// //         </>
-// //       )}
-
-// //       {/* Always show Footer */}
-// //       <Footer />
-// //     </div>
-// //   );
-// // };
-
-// // export default Root;
-
-
-// import React from 'react';
-// import { Outlet, useLocation } from 'react-router';
-// import Navbar from '../Components/Navbar';
-// import Footer from '../Components/Footer';
-// import Slider from '../Components/Slider';
-// import Carousel from '../Components/Carousel';
-// import BestSellingToys from '../Components/BestSellingToys';
-// import Testimonials from '../Components/Testimonials';
-// import HeroSection from '../Components/HeroSection';
-
-// const Root = () => {
-//   const location = useLocation(); // Get the current route
-
-//   // Define the routes where Slider, Carousel, BestSellingToys, and Testimonials should not be shown
-//   // Checking if path contains '/toy/' (for toy details pages)
-//   const hideLayoutComponents = ['/login', '/register', '/profile'];
-//   const isToyDetailsPage = location.pathname.includes('/toy/');
-
-//   return (
-//     <div>
-//       {/* Always show Navbar */}
-//       <Navbar />
-      
-//       {/* Show HeroSection only on the home page */}
-//       {location.pathname === '/' && <HeroSection />}
-
-//       {/* Conditionally show Slider and Carousel */}
-//       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
-//         <>
-//           <Slider />
-//           <Carousel />
-//         </>
-//       )}
-
-//       {/* Render the page content (Outlet) */}
-//       <Outlet />
-
-//       {/* Always show BestSellingToys and Testimonials, except on certain routes */}
-//       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
-//         <>
-//           <BestSellingToys />
-//           <Testimonials />
-//         </>
-//       )}
-
-//       {/* Always show Footer */}
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Root;
-
-
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
@@ -189,21 +9,15 @@ import Testimonials from '../Components/Testimonials';
 import HeroSection from '../Components/HeroSection';
 
 const Root = () => {
-  const location = useLocation(); // Get the current route
+  const location = useLocation();
 
-  // Define the routes where Slider, Carousel, BestSellingToys, and Testimonials should not be shown
   const hideLayoutComponents = ['/login', '/register', '/profile', '/forgot-password'];
   const isToyDetailsPage = location.pathname.includes('/toy/');
 
   return (
     <div>
-      {/* Always show Navbar */}
       <Navbar />
-      
-      {/* Show HeroSection only on the home page */}
       {location.pathname === '/' && <HeroSection />}
-
-      {/* Conditionally show Slider and Carousel */}
       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
         <>
           <Slider />
@@ -211,10 +25,8 @@ const Root = () => {
         </>
       )}
 
-      {/* Render the page content (Outlet) */}
       <Outlet />
 
-      {/* Always show BestSellingToys and Testimonials, except on certain routes */}
       {!hideLayoutComponents.includes(location.pathname) && !isToyDetailsPage && (
         <>
           <BestSellingToys />
@@ -222,7 +34,6 @@ const Root = () => {
         </>
       )}
 
-      {/* Always show Footer */}
       <Footer />
     </div>
   );
